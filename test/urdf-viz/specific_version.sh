ee862d22aac147b43862fdd42d921f4ba9ae9f9b
Austin Gregg-Smith <blooop@gmail.com>
2025-03-04 09:32:27 +0000

    fix version test

--- test/urdf-viz/specific_version.sh
+++ test/urdf-viz/specific_version.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "urdf-viz binary exists" bash -c "which urdf-viz"
 check "urdf-viz version command" bash -c "urdf-viz --version"
-check "specific version" bash -c "urdf-viz --version | grep 'v0.46.1'"
+check "specific version" bash -c "urdf-viz --version | grep '0.46.1'"
 
 # Report result
 reportResults
