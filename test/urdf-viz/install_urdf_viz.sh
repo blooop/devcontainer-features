8e0481b895c19dfb52b0db7e47b28050e1d73051
Austin Gregg-Smith <blooop@gmail.com>
2025-03-04 09:27:04 +0000

    update tests

--- test/urdf-viz/install_urdf_viz.sh
+++ test/urdf-viz/install_urdf_viz.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "urdf-viz binary exists" bash -c "which urdf-viz"
 check "urdf-viz version command" bash -c "urdf-viz --version"
-check "urdf-viz help command" bash -c "urdf-viz --help | grep -E 'Usage|Options'"
+check "urdf-viz help command" bash -c "urdf-viz --help | wc -l | test \$(cat) -gt 5"
 
 # Report result
 reportResults
--- test/urdf-viz/test.sh
+++ test/urdf-viz/test.sh
@@ -15,7 +15,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "urdf-viz binary exists" bash -c "which urdf-viz"
 check "urdf-viz version" bash -c "urdf-viz --version"
-check "urdf-viz help" bash -c "urdf-viz --help | grep -E 'Usage|Options'"
+check "urdf-viz help" bash -c "urdf-viz --help | wc -l | test \$(cat) -gt 5"
 
 # Report result
 reportResults
