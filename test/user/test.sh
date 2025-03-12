ff52315e49e3008922a59eb962b03bfb3cd3f7ec
Austin Gregg-Smith <blooop@gmail.com>
2025-03-12 19:02:43 +0000

    only have 1 test

--- test/user/test.sh
+++ test/user/test.sh
@@ -5,11 +5,9 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# Default username test
-check "default user exists" id dev
-# check "sudo access is configured" grep -q "dev ALL=" /etc/sudoers.d/dev
-check "default user home directory" test -d /home/dev
-# check "password is set" grep -q "dev:[^:]*:" /etc/shadow
-
+check "user exists" id dev
+check "sudo access is configured" sudo grep -q "dev ALL=" /etc/sudoers.d/dev
+check "user home directory" test -d /home/dev
+check "password is set" sudo grep -q "dev:[^:]*:" /etc/shadow
 # Report result
 reportResults
